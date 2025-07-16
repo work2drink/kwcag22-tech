2aa3d6c8563f17165a10a060a24b4cdc6f1d1549
resistan <resistan@gmail.com>
2025-07-16 15:18:47 +0900

    update images - working

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ export default defineConfig({
                 // }),
                 starlightFullViewMode({ leftSidebarEnabled: true, rightSidebarEnabled: true })
             ],
-            pagefind: false,
+            // pagefind: false,
             social: [{ icon: 'information', label: '원문 사이트', href: 'https://www.kioskui.or.kr/index.do?menu_id=00000976' }],
             components: {
                 Head: './src/components/CustomHead.astro',
@@ -133,7 +133,6 @@ export default defineConfig({
                 },
                 {
                     label: '견고성',
-                    link: '4'
                     collapsed: true,
                     items: [
                         {
